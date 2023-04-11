d201ddef2be85f793aa7ed844894a44398943e50
Mimi Rashid <21FTT1286@student.pb.edu.bn>
2023-04-11 21:36:12 +0800

    Install tailwind CSS

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,23 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+  <div class="md:flex">
+    <div class="md:shrink-0">
+      <img class="h-48 w-full object-cover md:h-full md:w-48" src="/src/assets/studentimage/HAMIMI_SICT_PHOTO.jpg" alt="MIMI">
+    </div>
+    <div class="p-8">
+      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Nur Hamimi Binti Haji Rashid</div>
+      <a href="https://pb.edu.bn/" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Politeknik Brunei</a>
+      <p class="mt-2 text-slate-500">Diploma in Web Development</p>
+
+      <br>
+
+      <a href=mailto:“21FTT1286@student.pb.edu.bn” class="text-gray-200 block rounded-lg text-center font-medium leading-6 px-6 py-3 bg-gray-900 hover:bg-black hover:text-white"><span class="font-bold">21FTT1286@student.pb.edu.bn</span></a>
+
+    </div>  
   </div>
-  <HelloWorld msg="Vite + Vue" />
+</div>
 </template>
 
 <style scoped>
@@ -28,3 +36,7 @@ import HelloWorld from './components/HelloWorld.vue'
   filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
+
+
+
+
